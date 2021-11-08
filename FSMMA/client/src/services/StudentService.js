03258c758e43d716e8e0e4bec98c30417163829c
sryaseer <rizz.yaseer@gmail.com>
2021-11-07 21:24:44 -0600

    added notes and studentcancel service

--- FSMMA/client/src/services/StudentService.js
+++ FSMMA/client/src/services/StudentService.js
@@ -1,38 +1,56 @@
-import axios from 'axios';
-const url = 'http://localhost:5562/';
+import axios from "axios";
+const url = "http://localhost:5562/";
 
 export default {
   viewMyInfo(id) {
-    return axios.get(url + 'student-view-info/' + id).then(response => response.data);
+    return axios
+      .get(url + "student-view-info/" + id)
+      .then((response) => response.data);
   },
 
   listGoals() {
     return axios
-      .get(url + 'get-fitness-goals/').then(response => response.data);
+      .get(url + "get-fitness-goals/")
+      .then((response) => response.data);
   },
 
   listSports() {
-    return axios
-      .get(url + 'get-sports/').then(response => response.data);
+    return axios.get(url + "get-sports/").then((response) => response.data);
   },
 
-  loadStudentSignups(){
+  loadStudentSignups() {
     return axios
-      .get(url + 'get-student-signups/').then(response => response.data);
+      .get(url + "get-student-signups/")
+      .then((response) => response.data);
   },
 
-  loadStudentSchedule(){
+  loadStudentSchedule() {
     return axios
-      .get(url + 'get-student-schedule/').then(response => response.data);
+      .get(url + "get-student-schedule/")
+      .then((response) => response.data);
   },
 
-  SignupStudentSchedule(){
+  SignupStudentSchedule() {
     return axios
-      .push(url + 'set-schedule-signup/').then(response => response.data);
+      .push(url + "set-schedule-signup/")
+      .then((response) => response.data);
   },
 
   viewStudentSchedule() {
-    return axios.get(url + 'student-view-schedule/').then(response => response.data);
+    return axios
+      .get(url + "student-view-schedule/")
+      .then((response) => response.data);
   },
 
+  studentCancelSignUp() {
+    return axios
+      .push(url + "student-cancels-signup/")
+      .then((response) => response.data);
+  },
+
+  studentAddNotes() {
+    return axios
+      .push(url + "add-student-note/")
+      .then((response) => response.data);
+  },
 };
